3fa80beb8ad014ffde3d29a8ad17eeda5f552f52
Muh Afrinal <afrinalhakim29@gmail.com>
2020-06-24 23:15:27 +0700

    Update Dan Menambahkan Splash Screen

--- app/src/main/java/com/rinal/covid19/MainActivity.kt
+++ app/src/main/java/com/rinal/covid19/MainActivity.kt
@@ -2,13 +2,11 @@ package com.rinal.covid19
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
---
+++ app/src/main/java/com/rinal/covid19/splash/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.rinal.covid19.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.rinal.covid19.MainActivity
+import com.rinal.covid19.R
+
+class SplashActivity : AppCompatActivity() {
+
+    private val timeSplash: Long = 1500
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(applicationContext, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, timeSplash)
+    }
+}
